c1ce7a249521b3850ba63851f84d47b7882149eb
Mohamed Khaled <mohamedleo404@gmail.com>
2024-03-06 20:12:23 +0200

    test stuff

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,9 @@ const nextConfig = {
       },
     ];
   },
+  images: {
+    domains: ["lh3.googleusercontent.com"],
+  },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+"use client";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { Button, Container, Typography } from "@mui/material";
+import Image from "next/image";
+
 export default function Home() {
+  const user = useCurrentUser();
+  const name = user?.name;
+  const email = user?.email;
+  const image = user?.image;
   return (
-    <main>
-      <h1>Welcome to JobFit</h1>
-      <p>The best place to find your next job!</p>
-    </main>
+    <Container>
+      {user ? (
+        <div>
+          <Typography variant="h6">Welcome back, {name}</Typography>
+          <p>Email: {email}</p>
+          <Image src={image || ""} alt="cll" width={50} height={50} />
+        </div>
+      ) : (
+        <div>
+          <Typography variant="h4">Welcome to our site</Typography>
+          <p>Please sign in</p>
+          <Button href="/auth/signin" variant="outlined">
+            Sign in
+          </Button>
+        </div>
+      )}
+    </Container>
   );
 }
