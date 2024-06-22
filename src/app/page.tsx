6bec31a34f48275a660f40cefdf720e3255c4fc8
M0Leo <mohamedleo404@gmail.com>
2024-06-22 09:16:20 +0300

    chore: edit home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,39 +5,50 @@ import { Container } from "@mui/material";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
 import SearchBar from "@/components/ui/SearchBar";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <NavBar />
-      <Container maxWidth="lg" component="main" style={{
-        height: "100vh",
-      }}>
-        <Box my={10}>
-          <Typography
-            variant="h3"
-            fontWeight="bold"
-            align-text=" left"
-            margin="auto"
-            color="black"
-          >
-            Discover<br></br>
-            more than
-          </Typography>
-          <Typography
-            variant="h2"
-            fontWeight="bold"
-            align-text=" left"
-            margin="auto"
-            color="primary"
-            font-weight="bold"
-          >
-            5000+ Jobs
-          </Typography>
-          <Typography variant="h5" color="#9e9d9d" align="left">
-            Great platform for the job seeker that searching <br></br>for new
-            career heights and passionate about startups.
-          </Typography>
+      <Container
+        maxWidth="lg"
+        component="main"
+        style={{
+          height: "100vh",
+        }}
+      >
+        <Box my={10} display="flex">
+          <Box>
+            <Typography
+              variant="h2"
+              fontWeight="bold"
+              align="left"
+              margin="auto"
+              color="black"
+            >
+              Elevate Your Career with
+            </Typography>
+            <Typography
+              variant="h2"
+              fontWeight="bold"
+              align="left"
+              margin="auto"
+              color="primary"
+            >
+              AI-Powered Matching
+            </Typography>
+            <Typography variant="h5" color="#9e9d9d" align="left">
+              Leverage cutting-edge AI technology to find personalized job
+              recommendations and make smarter career decisions.
+            </Typography>
+          </Box>
+          <Image
+            src="/images/career.svg"
+            alt="careerdraw"
+            width={500}
+            height={500}
+          />
         </Box>
         <SearchBar />
       </Container>
